760f0551b638aefa852c9cd9b3bf152bb2d63fc9
AkashDataScience <akashmshah19@gmail.com>
2024-05-26 15:02:34 +0530

    Updated EncoderBlock

--- model.py
+++ model.py
@@ -111,7 +111,7 @@ class EncoderBlock(nn.Module):
         super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connection = nn.ModuleList([ResidualConnection for _ in range(2)])
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
 
     def forward(self, x, src_mask):
         x = self.residual_connection[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
